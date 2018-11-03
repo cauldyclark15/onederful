08e772f674ca65f2ff0356dde8c03f61b733f397
cauldyclark15 <jcing.jc@gmail.com>
2018-11-04 00:40:38 +0800

    fix button error

--- src/Form.js
+++ src/Form.js
@@ -12,7 +12,7 @@ function SampleForm({ onSubmit }) {
     >
       <LabeledInput label="email" />
       <LabeledInput label="password" />
-      <Button htmlType="submit">submit</Button>
+      <button type="submit">submit</button>
     </form>
   );
 }
